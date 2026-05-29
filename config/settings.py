54373e46bd9c5867398874d185ff7416ef5bcc6f
gabrielbastosg <gabrielbastosg@gmail.com>
2026-05-29 15:56:19 -0300

    Refactor: SECRET_KEY/DEBUG no .env + CSS chip rule combinada

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u-m1=pzpck6a#lzl1#ee!ve&kh$k@wdh$-bz1s54$ri%766-$h'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
